e0cb205ef49b9279a2527b68ff4e3550525000d5
Olanipekun Adekunle <adekunle.olanipekun.ko@gmail.com>
2022-09-30 09:38:21 +0100

    global error controller

---
+++ Controllers/errorController.js
@@ -0,0 +1,87 @@
+const AppError = require("../utils/appError");
+
+// FUNCTION FOR HANDLING TOKEN AND MONGOOSE ERRORS
+const handleDuplicateErrorDB= (error)=>{
+    const message= `${error.keyValue.name} is taken already, please try another name` 
+    return new AppError(message, 400)  
+};
+
+const handleCastErrorDB= (error)=>{
+    const message= `Invalid ${error.path}: ${error.value}`
+    return new AppError(message, 400)
+}
+
+const handleValidationError= (error)=>{
+    const message= `${error.errors.name.path}, is either not provided or has the wrong value`
+    return new AppError(message, 400)
+}
+
+const handleJWTError= (error)=>{
+    const message= 'Please Log in'
+    return new AppError(message, 400)
+}
+
+const handleTokenExpired= (error)=>{
+    const message= 'Token Expired , Login again'
+    return new AppError(message, 400)
+}
+
+
+const sendErrorDev= (err,res)=>{
+    //development error: 
+    res.status(err.statusCode).json({
+        status: err.status, 
+        error: err,
+        message: err.message,
+        stack: err.stack
+    })
+};
+
+const sendProdErr= (err, res)=>{
+
+    // Error to be sent to client
+    if(err.isOperational){
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message,
+        })
+
+    //Hiding Programming error and sending a more friendly error to client
+    }else{
+    //Logging the error so the developer can see it, even when running in Production mode
+        console.error('ERROR💥', err)
+    //sendng the a friendlier error to client
+        res.status(500).json({
+            status: 'error',
+            message: 'Something went very wrong'
+        })
+    }
+}
+
+
+
+module.exports=(err, req, res, next)=>{
+    err.statusCode=  err.statusCode || 500
+    err.status= err.status || 'error'
+    
+    if(process.env.NODE_ENV==='development'){
+        sendErrorDev(err, res)
+    }else if (process.env.NODE_ENV==='production'){
+        
+        if(err.code=== 11000){
+            error= handleDuplicateErrorDB(err)
+        }else if(err.name==='CastError'){
+            error= handleCastErrorDB(err)
+        }else if(err.name==='ValidationError'){
+            error= handleValidationError(err)
+        }else if (err.name==='JsonWebTokenError'){
+            error= handleJWTError(err)
+        }else if(err.name==='TokenExpiredError'){
+            error= handleTokenExpired(err)
+        }else{
+            error=new AppError('something went wrong', 400)
+        }
+
+        sendProdErr(error, res)
+    }
+}
