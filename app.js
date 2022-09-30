7147ceb0417c7ccca08214d8874276a04633fe42
Olanipekun Adekunle <adekunle.olanipekun.ko@gmail.com>
2022-09-30 09:40:35 +0100

    adding error Handlers

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ const path= require('path')
 const bodyParser= require('body-parser')
 const userRouter= require('./Routes/userRoutes')
 
+//ERROR HANDLER
+const AppError= require('./utils/appError')
+const globalError= require('./Controllers/errorController')
+
 const app = express();
+
 
 app.use((req, res, next)=>{
     req.requestTime= new Date().toISOString();
@@ -12,5 +17,11 @@ app.use((req, res, next)=>{
 app.use(bodyParser.json());
 
 app.use('/api/v1/users', userRouter)
+
+app.all('*',(req, res, next)=>{
+    next (new AppError(`Page ${req.origianlUrl} is not found`, 404))
+})
+
+app.use(globalError)
 
 module.exports = app
