fd8d5bc7d650ff07d1ca8334d7f9e071e017415e
Olanipekun Adekunle <adekunle.olanipekun.ko@gmail.com>
2022-10-08 10:20:55 +0100

    modified class controller

--- Controllers/classController.js
+++ Controllers/classController.js
@@ -1,19 +1,25 @@
 const Class= require('../Models/classModel')
 const Course= require('../Models/courseModel')
+const Tutor= require('../Models/tutorModel')
 const AppError= require('../utils/appError')
 const catchAsync= require('../utils/catchAsync')
 const Email= require('../utils/email')
 
 
 exports.createClass= catchAsync(async(req, res, next)=>{
-    const course= await Course.findOne({'tutors.id': {$elemMatch: {id: req.params.tutorId}}});
-
-    console.log(course)
+    const course= await Course.findOne({
+        id: req.params.courseId,
+        tutors: {elemMatch:{
+            _id: req.params.tutorId
+        }}
+        
+    });
+    
 
     if (!course){
         return next(new AppError('no course with this Id and tutor', 404))
     }
-
+    console.log(req.user.id)
     const tutorial= await Class.create({
         students: req.user.id,
         tutor: req.params.tutorId,
@@ -27,6 +33,19 @@ exports.createClass= catchAsync(async(req, res, next)=>{
         data: tutorial
     })
 });
+
+exports.registerClass= catchAsync(async(req, res, next)=>{
+    console.log(req.user.id)
+    const instance= await Class.findByIdAndUpdate(req.params.classId, {$addToSet: {students:req.user.id}},{new: true})
+
+    console.log(instance)
+
+    res.status(200).json({
+        status: 'success',
+        data: instance
+    })
+    
+})
 
 exports.getAll= catchAsync(async(req, res, next)=>{
     const tutorials= await Class.find();
