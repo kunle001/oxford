df22adcfdad7680ab0dc21d2cc4ac4dd07eb85a7
Olanipekun Adekunle <adekunle.olanipekun.ko@gmail.com>
2022-10-11 22:19:48 +0100

    tutor functionalities updated

--- Controllers/tutorController.js
+++ Controllers/tutorController.js
@@ -10,15 +10,38 @@ exports.findOneTutor= catchAsync(async(req, res, next)=>{
     })
 });
 
-exports.updateTutor= catchAsync(async(req, res, next)=>{
-    const tutor= await Tutor.findByIdAndUpdate(req.params.tutorId, req.body, {
-        runValidators: true,
-        new: true
+const filterObj = (obj, ...allowedFields) => {
+    const newObj = {};
+    Object.keys(obj).forEach(el => {
+      if (allowedFields.includes(el)) newObj[el] = obj[el];
     });
-
+    return newObj;
+  };
+  
+  
+exports.updateProfile= catchAsync(async(req, res, next)=>{
+      if(req.body.password|| req.body.confirmPassword){
+          return next(new AppError('you cannot update password here', 400))
+      };
+  
+    // 2) Filtered out unwanted fields names that are not allowed to be updated
+    const filteredBody = filterObj(req.body, 'name', 'email');
+    if (req.file) filteredBody.photo = req.file.filename;
+  
+    // 3) Update user document
+    const updatedTutor = await Tutor.findByIdAndUpdate(req.user.id, filteredBody, {
+      new: true,
+      runValidators: true
+    });
+  
     res.status(200).json({
-        status: 'success',
-        data: tutor
-    })
-})
+      status: 'success',
+      data: {
+        user: updatedTutor
+      }
+    });
+  
+  });
+
+
 
--- Routes/tutorRoute.js
+++ Routes/tutorRoute.js
@@ -2,13 +2,19 @@ const express= require('express')
 const authController= require('../Controllers/authController')
 const tutorController= require('../Controllers/tutorController') 
 const reviewRouter= require('../Routes/reviewRoute')
+const userController= require('../Controllers/userController')
 
 const router= express.Router();
 router.use('/:tutorId/reviews', reviewRouter)
 
 router.route('/signup').post(authController.signUpTutor);
 router.route('/:tutorId').get(tutorController.findOneTutor)
-router.route('/update/:tutorId').patch(tutorController.updateTutor)
+
+router.route('/update-me').patch(
+    authController.protect,
+    userController.uploadUserPhoto,
+    userController.resizeUserPhoto,
+    tutorController.updateProfile)
 
 
 
